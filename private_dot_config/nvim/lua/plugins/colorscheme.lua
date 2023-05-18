e12be74e3f0c5fc3ab328ec071654b9dd0944484
Andrew Chon <andrewdchon@gmail.com>
2023-05-18 14:37:07 -0400

    fix: merge conflict

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- load the colorscheme here
-        vim.cmd([[colorscheme nordfox]])
-    end,
-}
